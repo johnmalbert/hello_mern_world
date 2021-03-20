d3e61a4ce936e54d9c76cc2b37ca141bf22b37e0
John Albert <john.m.albert@gmail.com>
2021-03-20 12:28:25 -0700

    added controller, config, model

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+require('./server/config/mongoose.config');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
 
 require('./server/routes/routes')(app);
 
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,16 @@
+const { Person } = require('../models/model');
 module.exports.index = (req, res) => {
     res.json( {
         message: "Hello World"
     })
+}
+
+module.exports.createPerson = (req, res) => {
+    const {firstName, lastName} = req.body;
+    Person.create({
+        firstName,
+        lastName
+    })
+        .then(Person => res.json(Person))
+        .catch(err => res.json(err))
 }
