d0c6cfed0d6eadf3b5d43f7ee5d0a275c8a1bd3c
John Albert <john.m.albert@gmail.com>
2021-03-20 16:50:04 -0700

    added form to views page, user can be created from browser!

---
+++ client/src/views/Main.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import PersonForm from '../Components/PersonForm';
+
+const Main = () => {
+    return (
+        <div>
+            <PersonForm />
+        </div>
+    )
+}
+
+export default Main
